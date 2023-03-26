e0950ac3042c38a383741ed9692fe6b67c026d00
Christin <caa8062@rit.edu>
2023-03-26 18:42:15 -0400

    Minor changes to make script work

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-$interface = $(ip link | awk -F: '$0 !~ "lo|vir|wl|^[^0-9]"{print $2;getline} | head -n 1)'
-$date= $(date "+%Y/%m/%d %H-%M-%S")
-tcpdump -i $interface -w $interface-$date.pcap
+interface=$(ip link | awk -F: '$0 !~ "lo|vir|wl|^[^0-9]"{print $2;getline}' | head -n 1 | xargs)
+date=$(date "+%Y-%m-%d_%H-%M-%S")
+tcpdump -i $interface -w $interface_$date.pcap
